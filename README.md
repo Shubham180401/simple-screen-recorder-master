60d7bdb7c9dfcdc718433dfa696699f1d7da55d6
Shubham <91934830+Shubham180401@users.noreply.github.com>
2023-07-19 20:19:12 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# simple-screen-recorder
+
+This is a simple screen recorder built using HTML, CSS, and JavaScript. It allows you to record your screen and save the video as an MP4 file.
+
+# Usage
+
+To use the screen recorder, simply click the "Start Recording" button to start recording your screen. You will be prompted to allow the website to access your screen and microphone. Once you give permission, the recording will start.
+
+While recording, you can click the "Stop Recording" button to stop the recording at any time. Once you stop the recording, the video will be displayed in the video player on the page. You can also click the "Download Video" button to download the video as an MP4 file.
+
+# Getting Started
+
+To get started with the Simple Screen Recorder, simply clone this repository and open the index.html file in your web browser. The screen recorder should be fully functional and ready to use.
+
+# Deployed Version
+
+You can try out this Screen Recorder on the following deployed website:
+
+https://adbspec.github.io/simple-screen-recorder/
---
+++ script.js
@@ -0,0 +1,49 @@
+
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const videoElement = document.getElementById('video-element');
+const downloadButton = document.getElementById('download-button');
+let mediaRecorder;
+let recordedChunks = [];
+
+startButton.addEventListener('click', () => {
+    navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
+
+            mediaRecorder.ondataavailable = e => {
+                recordedChunks.push(e.data);
+            };
+
+            mediaRecorder.onstop = () => {
+                const recordedBlob = new Blob(recordedChunks, { type: 'video/mp4' });
+                videoElement.src = URL.createObjectURL(recordedBlob);
+                recordedChunks = [];
+                downloadButton.disabled = false;
+                startButton.disabled = false;
+
+            };
+
+            mediaRecorder.start();
+            startButton.disabled = true;
+            stopButton.disabled = false;
+            downloadButton.disabled = true;
+        })
+        .catch(err => console.error('Error: ' + err));
+});
+
+stopButton.addEventListener('click', () => {
+    mediaRecorder.stop();
+    startButton.disabled = false;
+    stopButton.disabled = true;
+});
+
+downloadButton.addEventListener('click', () => {
+    const url = videoElement.src;
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'recorded-video.mp4';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+});
